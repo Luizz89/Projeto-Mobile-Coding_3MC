3eab89e92f99e426fff7f8b47d13cf1a3265399f
Luiz Gabryell <luizgabryell24@gmail.com>
2026-05-21 21:57:03 -0300

    duas pages
    criaçao de duas pages, e fazendo conexao entre elas, routerlink

---
+++ projeto/src/app/detalhes/detalhes.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-detalhes',
+  templateUrl: './detalhes.page.html',
+  styleUrls: ['./detalhes.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+})
+export class DetalhesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
