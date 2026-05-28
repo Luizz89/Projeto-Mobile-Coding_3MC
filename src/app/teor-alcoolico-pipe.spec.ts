7b5df550318f2810a6850775047d016b02890c9c
JFernandoValenca <jfv0111@gmail.com>
2026-05-28 18:09:34 -0300

    Delete src directory

--- src/app/teor-alcoolico-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TeorAlcoolicoPipe } from './teor-alcoolico-pipe';
-
-describe('TeorAlcoolicoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TeorAlcoolicoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
